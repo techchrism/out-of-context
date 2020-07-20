48b425de6491cb783686b8de65d0acea166a2559
Techdoodle <26680599+TheTechdoodle@users.noreply.github.com>
2020-07-19 18:44:43 -0700

    revise window share checker

--- src/components/TextShare.vue
+++ src/components/TextShare.vue
@@ -48,7 +48,7 @@
         computed: {
             canShare()
             {
-                return window.navigator.hasOwnProperty('share');
+                return !!window.navigator.share;
             }
         }
     };
